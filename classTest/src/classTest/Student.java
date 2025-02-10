87093459b31e680727f7138e5171b4ff25a82006
kys3189 <kimys31892@gmail.com>
2025-02-10 23:47:11 +0900

    day04-1

--- classTest/src/classTest/Student.java
+++ classTest/src/classTest/Student.java
@@ -5,39 +5,31 @@ import java.util.Scanner;
 public class Student {
 //	학생의 학번, 이름, 국어점수, 영어점수, 수학점수 입력 받고
 //	총점과 평균을 출력하기
+	
 	int studentNum;
 	String name;
-	int krScore;
-	int enScore;
-	int mtScore;
-	static int allScore;
-	int scoreAverage;
+	int kor;
+	int eng;
+	int math;
+	int total;
+	double average;
 	
 	public Student() {;}
 	
-	public Student(int studentNum, String name) {
+	public Student(int studentNum, String name, int kor, int eng, int math) {
 		this.studentNum = studentNum;
 		this.name = name;
-		this.scoreAverage = this.allScore / 3;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.total = kor + eng + math;
+		this.average = this.total / 3.0;
 	}
 	
 	public static void main(String[] args) {
 		
-		Student sd = new Student(20192019, "김영수");
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("국어 점수를 입력하세요.");
-		sd.krScore = sc.nextInt();
-		sd.allScore += sd.krScore;
-		System.out.println("영어 점수를 입력하세요.");
-		sd.enScore = sc.nextInt();
-		sd.allScore += sd.enScore;
-		System.out.println("수학 점수를 입력하세요.");
-		sd.mtScore = sc.nextInt();
-		sd.allScore += sd.mtScore;
-		
-		System.out.println(sd.allScore);
-		System.out.println(sd.scoreAverage);
+		Student 김영수 = new Student(1, "김영수", 63, 84, 55);
+		System.out.println("학점의 평균 : " + 김영수.average);
 		
 	}
 	
